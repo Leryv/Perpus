eaaefd653d27ba2e9395e354e7372a7b6771c22c
Andikaim <andikagamers50@gmail.com>
2021-12-01 08:24:24 +0700

    pisahin navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('','Perpus') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -21,90 +21,8 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light">
-            <div class="container">
-                <a class="navbar-brand font-weight-bold font-italic" href="{{ url('/') }}">
-                    {{ config('', 'Perpus') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a  href="{{route('home')}}" class="nav-link" > Home </a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a  href="{{route('books')}}" class="nav-link" >Buku</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a href="{{route('user')}}" class="nav-link" >User</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a href="{{route('card')}}" class="nav-link" >Cetak Kartu Anggota</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a href="{{route('category.create')}}" class="nav-link" >Category</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a  href="{{route('borrowings')}}" class="nav-link" >Peminjaman</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a  href="{{route('bookreturn')}}" class="nav-link" >Pengembalian</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a  href="{{route('laporan')}}" class="nav-link" >Laporan</a>
-                        </li>
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
+        <main class="">
+            @include('layouts.nav')
             @yield('content')
         </main>
     </div>
